76ad3723c2c598418ad0067f286ca255abc3be56
Takuro Matsukawa <takuro.matsukawa@gmail.com>
2021-07-31 21:45:49 +0900

    skelton screens

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
 <div class="p-10">
-  <h1 class="text-5xl text-gray-700">Dogs</h1>
+  <h1 class="text-5xl text-gray-700">Dogs give you all you need to know</h1>
   <div v-if="loaded" class="mt-10 flex flex-col gap-5">
-    <div v-for="image in images" :key="image" class="h-64">
-      <img :src="image" alt="dog" class="h-48"/>
-      <p class="text-gray-500 text-xs mt-1">{{image}}</p>
+    <div v-for="image in images" :key="image" class="h-64 w-64 border rounded">
+      <img :src="image" alt="dog" class="h-48 w-full object-cover object-top"/>
+      <div class="p-2">
+        <p class="text-gray-500 text-xs break-words">{{image}}</p>
+      </div>
+    </div>
+  </div>
+  <div v-else class="mt-10 flex flex-col gap-5 animate-pulse">
+    <div v-for="i in 3" :key="i" class="h-64 w-64 border rounded">
+      <div class="h-48 bg-gray-100 rounded"></div>
+      <div class="p-2">
+        <p class="text-gray-500 text-xs bg-gray-100 rounded h-5"></p>
+      </div>
     </div>
   </div>
 </div>
